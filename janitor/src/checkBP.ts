1fa01b7949b93522737d8bb54ff12aa363139199
ink0rr <tomflynnbeltsazar@gmail.com>
2023-04-24 19:20:55 +0700

    Optimize component group key checker

--- janitor/src/checkBP.ts
+++ janitor/src/checkBP.ts
@@ -1,7 +1,7 @@
-import { EntityBehavior, readJson, writeJson } from "https://deno.land/x/lazuli@0.0.4/mod.ts";
+import { EntityBehavior, Event, EventAction, readJson, writeJson } from "https://deno.land/x/lazuli@0.0.4/mod.ts";
 import glob from "npm:tiny-glob";
-import { getKeys } from "./getKeys.ts";
 import { cleanFiles } from "./cleanFiles.ts";
+import { getKeys } from "./getKeys.ts";
 
 export async function checkBP() {
   const keys = new Set<string>();
@@ -13,18 +13,30 @@ export async function checkBP() {
 
     if (!bp.component_groups) continue;
 
-    let changed = false;
-    const events = JSON.stringify(bp.events);
-    for (const key of Object.keys(bp.component_groups)) {
-      // Not sure how reliable using regex is, but it works for now
-      const match = events.match(RegExp(`(?<="component_groups":\\[.*)"${key}"(?=.*\\])`));
-      if (!match) {
-        changed = true;
-        console.log(`Unused component_group: "${key}" in ${path}`);
-        delete bp.component_groups[key];
+    const groupKeys = new Set(Object.keys(bp.component_groups));
+    const checkEventAction = (eventAction?: EventAction) => {
+      if (!eventAction) return;
+      for (const group of eventAction.component_groups ?? []) {
+        if (groupKeys.has(group)) {
+          groupKeys.delete(group);
+        }
       }
+    };
+    const checkEvent = (event: Event) => {
+      checkEventAction(event.add);
+      checkEventAction(event.remove);
+      event.sequence?.forEach(checkEvent);
+      event.randomize?.forEach(checkEvent);
+    };
+    for (const key in bp.events) {
+      checkEvent(bp.events[key]);
     }
-    if (changed) {
+
+    groupKeys.forEach((key) => {
+      console.warn(`Unused component group: "${key}" in ${path}`);
+      delete bp.component_groups![key];
+    });
+    if (groupKeys.size) {
       await writeJson(path, data);
     }
   }
