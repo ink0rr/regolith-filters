0ac0d75461e87507685cd003342d05616653287f
ink0rr <tomflynnbeltsazar@gmail.com>
2023-10-30 23:46:47 +0700

    Add esbuild deno filter

---
+++ esbuild_deno/README.md
@@ -0,0 +1,12 @@
+# Esbuild Deno
+
+Esbuild executor for Deno.
+
+## Settings
+
+| Name       | Description           | Default                  |
+| ---------- | --------------------- | ------------------------ |
+| configPath | deno.json config path | `./deno.json`            |
+| entryPoint | Entry point file path | `./data/scripts/main.ts` |
+| outfile    | Output file path      | `./BP/scripts/main.js`   |
+| minify     | Minify output         | `true`                   |
---
+++ esbuild_deno/main.ts
@@ -0,0 +1,39 @@
+import { resolve } from "https://deno.land/std@0.204.0/path/resolve.ts";
+import * as esbuild from "https://deno.land/x/esbuild@v0.19.4/mod.js";
+import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.2/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+
+const ROOT_DIR = Deno.env.get("ROOT_DIR")!;
+
+const {
+  configPath,
+  entryPoint,
+  outfile,
+  minify,
+} = z.object({
+  configPath: z.string().default("./deno.json").transform((path) => resolve(ROOT_DIR, path)),
+  entryPoint: z.string().default("./data/scripts/main.ts"),
+  outfile: z.string().default("./BP/scripts/main.js"),
+  minify: z.boolean().default(true),
+}).parse(JSON.parse(Deno.args[0] ?? "{}"));
+
+try {
+  await esbuild.build({
+    plugins: [...denoPlugins({ configPath })],
+    bundle: true,
+    entryPoints: [
+      entryPoint,
+    ],
+    external: [
+      "@minecraft/server",
+      "@minecraft/server-ui",
+    ],
+    format: "esm",
+    outfile,
+    minify,
+  });
+} catch (e) {
+  console.log(e.message);
+} finally {
+  esbuild.stop();
+}
