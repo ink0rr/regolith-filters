2eeb4f34ea62a44a19a99d804e95aca07a20c52e
ink0rr <tomflynnbeltsazar@gmail.com>
2024-01-29 15:12:28 +0700

    Update deps

--- esbuild_deno/main.ts
+++ esbuild_deno/main.ts
@@ -1,6 +1,6 @@
-import { resolve } from "https://deno.land/std@0.204.0/path/resolve.ts";
-import * as esbuild from "https://deno.land/x/esbuild@v0.19.4/mod.js";
-import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.2/mod.ts";
+import { resolve } from "https://deno.land/std@0.213.0/path/resolve.ts";
+import * as esbuild from "https://deno.land/x/esbuild@v0.20.0/mod.js";
+import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.5/mod.ts";
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 
 const ROOT_DIR = Deno.env.get("ROOT_DIR")!;
