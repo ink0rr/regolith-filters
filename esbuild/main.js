c1d684c01d2e536b7431f2d2f1a528d22562e7b2
ornariece <25489980+ornariece@users.noreply.github.com>
2025-12-18 20:18:06 +0100

    fix: allow outdir to override default outfile

--- esbuild/main.js
+++ esbuild/main.js
@@ -1,17 +1,18 @@
 import { build } from "esbuild";
 
 const args = process.argv[2];
+const parsedArgs = args ? JSON.parse(args) : {};
+
+// Remove conflicting default if outdir is provided
+const defaultConfig = {
+  bundle: true,
+  entryPoints: ["./data/scripts/main.ts"],
+  external: ["@minecraft/server", "@minecraft/server-ui"],
+  format: "esm",
+  ...(!parsedArgs.outdir && { outfile: "./BP/scripts/main.js" }),
+};
 
 await build({
-  bundle: true,
-  entryPoints: [
-    "./data/scripts/main.ts",
-  ],
-  external: [
-    "@minecraft/server",
-    "@minecraft/server-ui",
-  ],
-  format: "esm",
-  outfile: "./BP/scripts/main.js",
-  ...(args && JSON.parse(args)),
+  ...defaultConfig,
+  ...parsedArgs,
 });
