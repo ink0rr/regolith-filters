f477a81ea6330d4a9629ff12d5cad1f8887fbfd1
Beltsazar <tomflynnbeltsazar@gmail.com>
2025-12-19 14:59:39 +0700

    Bump esbuild version

--- esbuild/main.js
+++ esbuild/main.js
@@ -3,16 +3,14 @@ import { build } from "esbuild";
 const args = process.argv[2];
 const parsedArgs = args ? JSON.parse(args) : {};
 
-// Remove conflicting default if outdir is provided
-const defaultConfig = {
+const config = {
   bundle: true,
   entryPoints: ["./data/scripts/main.ts"],
   external: ["@minecraft/server", "@minecraft/server-ui"],
   format: "esm",
-  ...(!parsedArgs.outdir && { outfile: "./BP/scripts/main.js" }),
-};
-
-await build({
-  ...defaultConfig,
   ...parsedArgs,
-});
+};
+if (!config.outdir && !config.outfile) {
+  config.outfile = "./BP/scripts/main.js";
+}
+await build(config);
